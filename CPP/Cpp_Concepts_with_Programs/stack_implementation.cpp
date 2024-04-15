d34588ffc91bc44aa39bdde63aa9205bc9f88351
Ranjit-Singh-786 <jiradhey402@gmail.com>
2024-04-16 02:05:23 +0530

    stack

---
+++ CPP/Cpp_Concepts_with_Programs/stack_implementation.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_SIZE 100
+
+class Stack{
+    private:
+    int top;
+    int arr[MAX_SIZE];
+
+    public:
+    Stack(){
+        top = -1;
+    }
+
+
+    void push(int value){
+        if (top == MAX_SIZE-1){
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+        arr[++top] = value;
+        cout<<"succussfully inserted :"<<value<<endl;
+    }
+
+
+    void pop(){
+        if (isEmpty()){
+            cout<<"Stack underflow";
+        }
+        top--;
+
+    }
+
+    int peak(){
+        if(isEmpty()){
+            return -1;
+        }
+        return arr[top];
+    }
+
+    bool isEmpty(){
+        return top == -1;
+    }
+
+void display() {
+    cout << "Stack elements: ";
+    for (int i = 0; i <= top; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+};
+
+int main(){
+    Stack st;
+    st.push(10);
+    st.push(15);
+    st.push(20);
+
+    cout<<"Your PEAK is : "<<st.peak()<<endl;
+    cout<<endl;
+
+    st.pop();
+    cout<<endl;
+    cout<<"Your PEAK is after Pop operation : "<<st.peak()<<endl;
+
+    cout<<"Traversing data item from stack"<<endl;
+    st.display();
+    return 0;
+}
